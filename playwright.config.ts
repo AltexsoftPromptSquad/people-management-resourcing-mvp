f38d8ac48b1bdc179dd11695bb80cd4136b5ed5f
ivan.zamikula <ivan.zamikula@altexsoft.com>
2026-07-06 14:35:32 +0300

    Updated ci.yml and playwright.config to support parallel jobs

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   reporter: [['list'], ['html', { outputFolder: 'tests/e2e/.report', open: 'never' }]],
   use: {
     baseURL,
